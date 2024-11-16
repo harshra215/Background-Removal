5d3d84bbbce6689dd056f9c4453bab6a08c0475e
Rupak swar <116864759+ai-rupak@users.noreply.github.com>
2024-11-16 22:00:28 +0530

    Webhooks Added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 // App Config
 const PORT = process.env.PORT || 4000
@@ -15,5 +16,6 @@ app.use(cors())
 
 // Api Routes 
 app.get('/' , (req,res) =>res.send("API Working"))
+app.use('/api/user',userRouter)
 
 app.listen(PORT , ()=> console.log("Server Running on port" +PORT))
