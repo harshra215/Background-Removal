d1e0b505755e59da8d2388fa4de8fc969f8bc1cd
Rupak swar <116864759+ai-rupak@users.noreply.github.com>
2024-11-21 23:26:06 +0530

    Clipdrop Api integrated

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 // App Config
 const PORT = process.env.PORT || 4000
@@ -17,5 +18,6 @@ app.use(cors())
 // Api Routes 
 app.get('/' , (req,res) =>res.send("API Working"))
 app.use('/api/user',userRouter)
+app.use('/api/image', imageRouter)
 
 app.listen(PORT , ()=> console.log("Server Running on port " +PORT))
